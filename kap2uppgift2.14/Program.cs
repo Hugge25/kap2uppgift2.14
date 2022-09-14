39021e89d13adad8fa9ce39ed4db7e53d36493bc
Admin <Admin@TEB123014>
2022-09-14 11:28:39 +0200

    Add project files.

---
+++ kap2uppgift2.14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Transactions;
+
+namespace uppgfit_2._14
+{
+    class Porgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett decimal tal");
+            double tal1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in ett till decimal tal");
+            double tal2 = double.Parse(Console.ReadLine());
+
+            double produkt = (tal1 * tal2);
+
+            Console.WriteLine("Produkten av " + tal1 + " och " + tal2 + " är lika med "  + produkt);
+
+        }
+   
+  
+    }
+}
